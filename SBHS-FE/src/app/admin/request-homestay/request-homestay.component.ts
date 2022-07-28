abd949fbbc3d5635c54e1bb788890a32e82b641b
nguyenanhquan0 <quan251299@gmail.com>
2022-07-29 03:00:49 +0700

    done verify homestay

--- SBHS-FE/src/app/admin/request-homestay/request-homestay.component.ts
+++ SBHS-FE/src/app/admin/request-homestay/request-homestay.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServerHttpService } from 'src/app/services/verify-homestay.service';
 
 @Component({
   selector: 'app-request-homestay',
@@ -6,10 +7,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./request-homestay.component.scss']
 })
 export class RequestHomestayComponent implements OnInit {
+  values : data[] = [];
+  
+  // displayedColumns: string[] = ['id', 'createdBy', 'createdDate', 'type', 'status', 'actions'];
+  // dataSource= new MatTableDataSource(this.values);
 
-  constructor() { }
 
+  // @ViewChild(MatPaginator) paginator!: MatPaginator;
+  // @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private http: ServerHttpService) {
+  } 
   ngOnInit(): void {
+    this.http.getLanlord().subscribe((data =>{
+      this.values = data;
+      console.log(this.values)
+    }),
+    error =>{
+      alert(error)
+    })
   }
+  public Id = 0;
+  public isAccept = true;
+  public isReject = false;
+  public rejectMessage = "";
+  public onItemSelector(value :number) {
+        this.Id=value;
+    }
+  public accept(){
+    this.http.verifyLandlord(this.Id +"",this.isAccept,this.rejectMessage).subscribe((data =>{
+      if(data !=null){
+        location.reload();
+      }
+      
+      console.log(data)
+    }),
+    error =>{
+      alert(error)
+    })
+  }
+  public reject(){
+    this.http.verifyLandlord(this.Id +"",this.isReject,this.rejectMessage).subscribe((data =>{
+      if(data !=null){
+        location.reload();
+      }
+    }),
+    error =>{
+      alert(error)
+    })
+  }
+
+}
+
+
+export interface data {
+  createdBy: string;
+  id: number;
+  createdDate: string;
+  type: string;
+  status: string;
 
 }
